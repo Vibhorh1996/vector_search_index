531acee7f451e1878072e75f91858dc9652866b3
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-24 11:32:21 +0530

    Update demo_update.py

--- demo_update.py
+++ demo_update.py
@@ -4,100 +4,125 @@ import PyPDF2
 import streamlit as st
 from pathlib import Path
 import faiss
+from src.parse_document import PdfParser
+from src.indexer import FaissIndexer
 import openai
 
-# Set OpenAI API key
-openai_api_key = st.secrets["openai_api_key"]
-openai.api_key = openai_api_key
-
-# ChatGPT prompt message
-prompt_message = """
-<Chat GPT, I want you to act as a Conversational AI Expert wherein a user will ask you questions, based on some information which the user will provide you. You need to answer the questions ONLY based on the information provided by the user, in at most 100 words.
-
-Requirement from the User will be to upload the information, which can be in paragraph form, points, or any other format.
-
-You will first prompt the user - 'Provide me with the information on which you want to ask questions on:'. Then the user will provide the information.
-
-As the information is given to you, you will prompt the user - 'Understood. You may now ask your question.' You need to answer the question ONLY based on the information provided to you by the user.
-
-Once you have provided the answer, in the next line within the answer only, you will prompt the user - 'Do you have any other questions on this topic?' Provide options (Y/N).
-
-- If the user selects 'Y' or 'y', prompt the user - 'Continue with this topic or New Query?' and provide options (Current/New).
-    - If the user selects 'Current' (case insensitive), prompt the user - 'Understood. You may now ask your question.' Repeat this until the user selects 'Current' (case insensitive). Make sure you ONLY answer the questions of the user based on the information which the user provided you at the first place and nothing outside of that.
-    - If the user replies with 'New' (case insensitive), prompt the user - 'Understood. Please provide me with a new set of information'. At this point, the user is expected to provide new information to you. After the information is provided to you, prompt the user - 'Understood. You may now ask your question.' Again, make sure that you answer the question ONLY based on the information provided to you by the user.
-
-- If the user asks a question outside of the provided information, prompt the user - 'Please ask questions relevant to the given information only.' and ask again - 'Do you have any other questions?' Provide options (Y/N).
-- If the user replies with 'N' or 'n', prompt the user - 'Hope you got the answer you were looking for.'
-
-Repeat the process as per the user input from the above points.
+"""
+This is a Streamlit-based application that works as a frontend for building a vector search index.
 """
 
-# Function to generate chatbot response using OpenAI's ChatGPT
-def generate_chatbot_response(message, chat_history=None):
-    if chat_history is None:
-        chat_history = []
-    
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt_message + "\nQ: " + message + "\nA:",
-        temperature=0.7,
-        max_tokens=50,
-        n=1,
-        chat_history=chat_history,
-    )
-    
-    chat_history.append({"role": "system", "content": "Q: " + message + "\nA: " + response.choices[0].text.strip()})
-    
-    return response.choices[0].text.strip(), chat_history
-
-# Remaining code...
+# Set OpenAI API key using Streamlit text input
+openai_api_key = st.text_input("Enter your OpenAI API key:", type="password")
+if not openai_api_key:
+    st.warning("Please enter your OpenAI API key to proceed.")
+else:
+    openai.api_key = openai_api_key
 
 # Set page title and header using Streamlit Markdown
 st.markdown("# PDF Parser and Search")
-# ...
+st.markdown("Upload one or more PDF files and click the 'Parse' button to parse them and dump the extracted data as JSON. "
+            "Then, click the 'Build Index' button to create a Faiss index from the generated JSON files. "
+            "Finally, enter a search query and click the 'Search' button to interact with the chatbot.")
+
+# Create a file uploader for multiple files
+uploaded_files = st.file_uploader("Choose one or more PDF files", type="pdf", accept_multiple_files=True)
+
+# Create a button to start parsing
+if st.button("Parse"):
+    if uploaded_files:
+        summaries = []  # Store summaries for each PDF file
+        for uploaded_file in uploaded_files:
+            # Create a PdfParser object for each uploaded file
+            pdf_parser = PdfParser(uploaded_file)
+
+            # Parse the PDF file
+            pdf_parser.parse_pdf()
+
+            # Write the extracted data to a JSON file with the same name as the PDF file
+            input_filename = Path(uploaded_file.name)
+            output_filename = input_filename.stem + ".json"
+            pdf_parser.write_json(output_filename)
+
+            # Extract a short brief from the PDF
+            # Modify this section to extract the summary/brief from the parsed PDF
+            summary = "Summary: [Summary of the PDF in at most 50 words]"
+            summaries.append((input_filename, summary))
+
+            # Display a success message
+            st.success(f"Extracted data from {input_filename} has been written to {output_filename}.")
+    else:
+        # Display an error message if no file was uploaded
+        st.error("Please upload one or more PDF files.")
+
+# Create a button to build the Faiss index
+if st.button("Build Index"):
+    # Define the list of JSON files to index
+    json_files = [str(Path(file.name).stem) + '.json' for file in uploaded_files]
+
+    # Create a Faiss indexer object and build the index
+    indexer = FaissIndexer(json_files)
+    indexer.build_index()
+
+    if indexer:
+        indexer.save_index('./tmp.index')
+        st.markdown("**:blue[ Faiss index has been built and stored at: tmp.index]**")
 
 # Create a search query input box and search button
 query = st.text_input("Enter a search query:")
 if st.button("Search"):
-    if indexer:
-        # Search the index using the query
+    # Search the index using the query
+    indexer = FaissIndexer.load_index('./tmp.index')  # Load the index and assign it to the indexer variable
+
+    if indexer:  # Check if index was successfully loaded
+        st.markdown('**:blue[Loaded index from: tmp.index]**')
         D, I, search_results = indexer.search_index(query)  # Get distances, indices, and search results
 
-        chat_history = []  # Initialize chat history for the chatbot
-        user_question = query
-        st.markdown("Q. " + user_question)  # Display user's question
+        # Start the conversation with the chatbot
+        st.write("Q:", query)
 
-        while True:
-            # Generate chatbot response
-            chatbot_answer, chat_history = generate_chatbot_response(user_question, chat_history)
-            st.markdown("A. " + chatbot_answer)  # Display chatbot's answer
+        information_provided = st.text_area("Provide me with the information on which you want to ask questions on:")
 
-            if re.search(r"\bnew\b", chatbot_answer, re.IGNORECASE):
-                st.warning("Please provide a new set of information.")
-                user_information = st.text_area("New Information:")
-                st.success("Understood. You may now ask your question.")
-                user_question = st.text_input("Enter your question:")
-                chat_history = []  # Reset chat history for new information
-            elif re.search(r"\bcurrent\b", chatbot_answer, re.IGNORECASE):
-                st.success("Understood. You may now ask your question.")
-                user_question = st.text_input("Enter your question:")
-            else:
-                continue_prompt = st.radio("Do you have any other questions on this topic?", ("Yes", "No"))
-                if continue_prompt.lower() == "yes":
-                    new_current_prompt = st.radio("Continue with this topic or new query?", ("Current", "New"))
-                    if new_current_prompt.lower() == "current":
-                        st.success("Understood. You may now ask your question.")
-                        user_question = st.text_input("Enter your question:")
-                    else:
-                        st.warning("Please provide a new set of information.")
-                        user_information = st.text_area("New Information:")
-                        st.success("Understood. You may now ask your question.")
-                        user_question = st.text_input("Enter your question:")
+        if not information_provided:
+            st.error("Please provide the necessary information.")
+        else:
+            continue_topic = True
+            while continue_topic:
+                st.write("A: [Chat GPT, I want you to act as a Conversational AI Expert wherein a user will ask you questions, based on some information which the user will provide you. You need to answer the questions ONLY based on the information provided by the user, in at most 100 words.]")
+
+                # Chatbot responds to the user's question based on the provided information
+                answer = openai.Completion.create(
+                    engine="text-davinci-003",
+                    prompt=f"Q: {query}\nA: {information_provided}\n",
+                    max_tokens=100,
+                    n=1,
+                    stop=None,
+                    temperature=0.7,
+                )
+
+                st.write("A:", answer.choices[0].text.strip())
+
+                another_question = st.selectbox("Do you have any other questions on this topic?", ("Yes", "No"))
+
+                if another_question.lower() == "no":
+                    st.write("A: Hope you got the answer you were looking for.")
+                    continue_topic = False
                 else:
-                    st.markdown("Hope you got the answer you were looking for.")
-                    break
+                    continue_topic_input = st.selectbox("Continue with this topic or new query?", ("Current", "New"))
 
+                    if continue_topic_input.lower() == "new":
+                        information_provided = st.text_area("Understood. Please provide me with a new set of information:")
+                    else:
+                        st.write("A: Understood. You may now ask your question.")
+                        query = st.text_input("Enter a search query:")
     else:
         st.error("Failed to load index. Please make sure the index has been built.")
 
-# ...
+# Uncomment the code below if you want to display summaries
+# show_summaries = st.checkbox("Show Summaries")
+# if show_summaries and uploaded_files:
+#     for i, (input_filename, summary) in enumerate(summaries):
+#         st.write(f"PDF: {input_filename}")
+#         st.write(summary)
+#         if i < len(uploaded_files) - 1:  # Add a horizontal line if it's not the last file
+#             st.markdown("---")
