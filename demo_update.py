6b90d07d0b6d0ed6fe229ebd931697e3f8eff8ca
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-24 11:44:30 +0530

    Update demo_update.py

--- demo_update.py
+++ demo_update.py
@@ -4,26 +4,33 @@ import PyPDF2
 import streamlit as st
 from pathlib import Path
 import faiss
+import openai
 from src.parse_document import PdfParser
 from src.indexer import FaissIndexer
-import openai
 
 """
 This is a Streamlit-based application that works as a frontend for building a vector search index.
 """
 
-# Set OpenAI API key using Streamlit text input
-openai_api_key = st.text_input("Enter your OpenAI API key:", type="password")
-if not openai_api_key:
-    st.warning("Please enter your OpenAI API key to proceed.")
-else:
-    openai.api_key = openai_api_key
-
 # Set page title and header using Streamlit Markdown
+# st.set_page_config(page_title="PDF Parser and Search", page_icon=":mag:", layout="centered")
 st.markdown("# PDF Parser and Search")
 st.markdown("Upload one or more PDF files and click the 'Parse' button to parse them and dump the extracted data as JSON. "
             "Then, click the 'Build Index' button to create a Faiss index from the generated JSON files. "
-            "Finally, enter a search query and click the 'Search' button to interact with the chatbot.")
+            "Finally, enter a search query and click the 'Search' button to perform a search on the Faiss index.")
+
+# Set the OpenAI API key using Streamlit text input
+openai_api_key = st.text_input("Enter your OpenAI API key:", type="password")
+if not openai_api_key:
+    st.warning("Please enter your OpenAI API key to proceed.")
+
+# # Create checkboxes to select the GPT model
+# use_gpt35 = st.checkbox("Use GPT-3.5", value=True)
+# use_gpt4 = st.checkbox("Use GPT-4")
+
+# Create radio buttons to select the GPT model
+selected_model = st.radio("Select GPT model:", options=["GPT-3.5", "GPT-4"])
+st.write(f"Selected GPT model: {selected_model}")
 
 # Create a file uploader for multiple files
 uploaded_files = st.file_uploader("Choose one or more PDF files", type="pdf", accept_multiple_files=True)
@@ -70,59 +77,133 @@ if st.button("Build Index"):
 
 # Create a search query input box and search button
 query = st.text_input("Enter a search query:")
-if st.button("Search"):
-    # Search the index using the query
-    indexer = FaissIndexer.load_index('./tmp.index')  # Load the index and assign it to the indexer variable
+
+# Check if the user has entered a query and the Faiss index exists
+if query and Path('./tmp.index').is_file():
+    # Load the Faiss index
+    indexer = FaissIndexer.load_index('./tmp.index')
 
     if indexer:  # Check if index was successfully loaded
         st.markdown('**:blue[Loaded index from: tmp.index]**')
         D, I, search_results = indexer.search_index(query)  # Get distances, indices, and search results
 
-        # Start the conversation with the chatbot
-        st.write("Q:", query)
+        # Store the search results to be used in the conversation
+        conversation_info = {'query': query, 'search_results': search_results}
 
-        information_provided = st.text_area("Provide me with the information on which you want to ask questions on:")
+        # Display the search results
+        for i, result in enumerate(search_results):
+            st.write(f"Result {i+1}: {result}")
+            if i < len(search_results) - 1:  # Add a horizontal line if it's not the last result
+                st.markdown("---")
+            # Display additional details about the search result if needed
 
-        if not information_provided:
-            st.error("Please provide the necessary information.")
-        else:
-            continue_topic = True
-            while continue_topic:
-                st.write("A: [Chat GPT, I want you to act as a Conversational AI Expert wherein a user will ask you questions, based on some information which the user will provide you. You need to answer the questions ONLY based on the information provided by the user, in at most 100 words.]")
+        # Prompt the user for information
+        st.text("Provide me with the information on which you want to ask questions on:")
+        information = st.text_area("")
 
-                # Chatbot responds to the user's question based on the provided information
-                answer = openai.Completion.create(
-                    engine="text-davinci-003",
-                    prompt=f"Q: {query}\nA: {information_provided}\n",
-                    max_tokens=100,
-                    n=1,
-                    stop=None,
-                    temperature=0.7,
-                )
+        # Check if the user has provided information
+        if information:
+            st.text("Understood. You may now ask your question.")
 
-                st.write("A:", answer.choices[0].text.strip())
+            # Create a conversation list to store the user and AI responses
+            conversation = []
 
-                another_question = st.selectbox("Do you have any other questions on this topic?", ("Yes", "No"))
+            # Start the conversation loop
+            while True:
+                user_question = st.text_input("User:")
+                if user_question:
+                    # Add the user question to the conversation
+                    conversation.append(user_question)
 
-                if another_question.lower() == "no":
-                    st.write("A: Hope you got the answer you were looking for.")
-                    continue_topic = False
-                else:
-                    continue_topic_input = st.selectbox("Continue with this topic or new query?", ("Current", "New"))
+                    # Check if the user question is related to the provided information
+                    if user_question.strip().endswith("?") and information.lower() in user_question.lower():
+                        # Get the AI response based on the user question
+                        ai_response = get_ai_response(user_question, conversation_info, selected_model, openai_api_key)
 
-                    if continue_topic_input.lower() == "new":
-                        information_provided = st.text_area("Understood. Please provide me with a new set of information:")
+                        # Add the AI response to the conversation
+                        conversation.append(ai_response)
+
+                        # Display the AI response
+                        st.text("AI:" + ai_response)
+
+                        # Check if the conversation should continue
+                        continue_conversation = st.selectbox("Do you have any other questions on this topic?", options=["Yes", "No"])
+
+                        if continue_conversation.lower() == "no":
+                            st.text("Hope you got the answer which you were looking for.")
+                            break
+
+                        # Check if the user wants to continue with the current topic or start a new query
+                        continue_topic = st.selectbox("Continue with this topic or new query?", options=["Current", "New"])
+
+                        if continue_topic.lower() == "new":
+                            st.text("Understood. Please provide me with a new set of information.")
+                            information = st.text_area("")
+                            conversation_info['search_results'] = []  # Clear the search results for the new information
+                            conversation = []  # Clear the conversation for the new information
+                            st.text("Understood. You may now ask your question.")
+                        else:
+                            st.text("Understood. You may now ask your question.")
                     else:
-                        st.write("A: Understood. You may now ask your question.")
-                        query = st.text_input("Enter a search query:")
+                        st.text("Please ask questions relevant to the given information only.")
+                else:
+                    st.text("Please ask a question.")
+
     else:
         st.error("Failed to load index. Please make sure the index has been built.")
+else:
+    st.text("Please enter a search query.")
 
-# Uncomment the code below if you want to display summaries
-# show_summaries = st.checkbox("Show Summaries")
-# if show_summaries and uploaded_files:
-#     for i, (input_filename, summary) in enumerate(summaries):
-#         st.write(f"PDF: {input_filename}")
-#         st.write(summary)
-#         if i < len(uploaded_files) - 1:  # Add a horizontal line if it's not the last file
-#             st.markdown("---")
+# Function to get AI response
+def get_ai_response(question, conversation_info, selected_model, openai_api_key):
+    context = f"Question: {question}\nInformation: {conversation_info['search_results']}"
+    prompt = f"<Chat GPT, i want you to act as a Conversational AI Expert wherein a user will ask you questions, based on some information which the user will provide you. You need to answer the questions ONLY based on the information provided by the user, in at most 100 words.\n\nUser Question: {question}\n\nAnswer:"
+
+    # Send the prompt to OpenAI API to get the AI response
+    ai_response = generate_ai_response(prompt, context, selected_model, openai_api_key)
+
+    # Extract the answer from the AI response
+    answer = extract_answer(ai_response)
+
+    return answer
+
+# Function to generate AI response using OpenAI API
+def generate_ai_response(prompt, context, selected_model, openai_api_key):
+    # Set the GPT model name
+    model_name = "gpt-3.5-turbo" if selected_model == "GPT-3.5" else "gpt-4.0-turbo"
+
+    # Set the OpenAI API endpoint
+    api_endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions" if model_name == "gpt-4.0-turbo" else "https://api.openai.com/v1/engines/davinci/completions"
+
+    # Set the OpenAI API headers
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai_api_key}"
+    }
+
+    # Set the OpenAI API data
+    data = {
+        "prompt": prompt,
+        "max_tokens": 100,
+        "temperature": 0.5,
+        "top_p": 1.0,
+        "n": 1,
+        "stop": None,
+        "context": context,
+    }
+
+    # Send the request to OpenAI API and get the response
+    response = requests.post(api_endpoint, headers=headers, json=data)
+    response_json = response.json()
+
+    # Extract the AI response from the API response
+    ai_response = response_json['choices'][0]['text']
+
+    return ai_response
+
+# Function to extract the answer from AI response
+def extract_answer(ai_response):
+    # Remove the prompt and leading/trailing whitespaces from the AI response
+    answer = ai_response.split("Answer:")[1].strip()
+
+    return answer
